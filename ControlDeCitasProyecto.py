116fdd7df6aeea598f286495d1836667e5fb1d9d
DeLaCruzHernandezJessica <jessica.delacruz.cbtis03@gmail.com>
2025-05-23 19:16:51 -0600

    Update ControlDeCitasProyecto.py

--- ControlDeCitasProyecto.py
+++ ControlDeCitasProyecto.py
@@ -1,19 +1,1 @@
-import tkinter as tk
-ventana=tk.Tk()
-ventana.title("area")
-ventana.geometry("400x300")
 
-etiqueta=tk.Label(ventana, text="ingresa el radio del circulo")
-etiqueta.pack()
-entrada=tk.Entry(ventana)
-radio=(entrada.get())
-entrada.pack()
-
-area=(3.14*(radio*radio))
-boton=tk.Button(ventana, text="resultado",)
-boton.pack()
-etiqueta2=tk.Label(ventana, text=f"el area es {area}")
-etiqueta2.pack()
-
-
-ventana.mainloop()

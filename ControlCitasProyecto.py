3bddbf33c9c8257e2f96fd9ece59b0eaf3463edb
DeLaCruzHernandezJessica <jessica.delacruz.cbtis03@gmail.com>
2025-05-30 23:01:10 -0600

    ControlCitasProyecto.py

--- ControlCitasProyecto.py
+++ ControlCitasProyecto.py
@@ -8,21 +8,29 @@ ventana.config(bg="gray")
 letrero=tk.Label(ventana, text="Bienvenido", font=("Times New Roman", 40)).pack(pady=20)
 
 class registrar:
-    def __init__(self, nombre, apellido1, apellido2, contraseña):
+    def __init__(self, nombre, apellido1, apellido2, curp, sexo, edad, telefono, cargo):
         self.nombre = nombre
         self.apellido1 = apellido1
         self.apellido2 = apellido2
-        self.contraseña = contraseña
+        self.curp = curp
+        self.sexo = sexo
+        self.edad = edad
+        self.telefono = telefono
+        self.cargo = cargo
     def datos(self):
-        return f"Nombre completo: {self.nombre} {self.apellido1} {self.apellido2}"
+        return f"{self.nombre} {self.apellido1} {self.apellido2} a sido registrado exitosamente"
 
 def registrar_usuario():
  nombre = en_nombre.get()
  apellido1 = en_apellido1.get()
  apellido2 = en_apellido2.get()
- contraseña = en_contraseña.get()
+ curp = en_curp.get()
+ sexo = en_sexo.get()
+ edad = en_edad.get()
+ telefono = en_telefono.get()
+ cargo = en_cargo.get()
 
- registro = registrar(nombre, apellido1, apellido2, contraseña)
+ registro = registrar(nombre, apellido1, apellido2, curp, sexo, edad, telefono, cargo)
  mensaje = registro.datos()
  messagebox.showinfo("Datos Registrados", mensaje)
 
@@ -30,25 +38,44 @@ def mostrar_formulario(event):
     seleccion = combo.get()
     if seleccion == "Registrarse":
 
-     tk.Label(ventana,text="Nombre: ", font=("Calibri", 20), bg="gray", width=10,).pack(pady=0)
+     tk.Label(ventana,text="Nombre: ", font=("Calibri", 10), bg="gray").pack(pady=0)
      global en_nombre
-     en_nombre=tk.Entry(ventana, width=50, font=("Calibri", 15))
+     en_nombre=tk.Entry(ventana, font=("Calibri", 10))
      en_nombre.pack(pady=8)
 
-     tk.Label(ventana, text="Apellido paterno: ", font=("Calibri", 20), bg="gray").pack()
+     tk.Label(ventana, text="Apellido paterno: ", font=("Calibri", 10), bg="gray").pack()
      global en_apellido1
-     en_apellido1=tk.Entry(ventana, font=("Calibri", 15))
+     en_apellido1=tk.Entry(ventana, font=("Calibri", 10))
      en_apellido1.pack(pady=8)
 
-     tk.Label(ventana, text="Apellido materno: ", font=("Calibri", 20), bg="gray").pack()
+     tk.Label(ventana, text="Apellido materno: ", font=("Calibri", 10), bg="gray").pack()
      global en_apellido2
-     en_apellido2=tk.Entry(ventana, font=("Calibri", 15))
+     en_apellido2=tk.Entry(ventana, font=("Calibri", 10))
      en_apellido2.pack(pady=8)
 
-     tk.Label(ventana, text="Contraseña: ",font=("Calibri", 20), bg="gray").pack()
-     global en_contraseña
-     en_contraseña=tk.Entry(ventana, show="*", font=("Calibri", 15))
-     en_contraseña.pack(pady=8)
+     tk.Label(ventana, text="CURP: ",font=("Calibri", 10), bg="gray").pack()
+     global en_curp
+     en_curp=tk.Entry(ventana, show="*", font=("Calibri", 10))
+     en_curp.pack(pady=8)
+
+     valor=tk.StringVar()
+     global en_sexo
+     en_sexo=tk.OptionMenu(ventana, valor, "masculino", "femenino", "otro").pack
+
+     tk.Label(ventana, text="Edad: ",font=("Calibri", 10), bg="gray").pack()
+     global en_edad
+     en_edad=tk.Entry(ventana, font=("Calibri", 10))
+     en_edad.pack(pady=8)
+
+     tk.Label(ventana, text="Telefono: ",font=("Calibri", 10), bg="gray").pack()
+     global en_telefono
+     en_telefono=tk.Entry(ventana, show="*", font=("Calibri", 10))
+     en_telefono.pack(pady=8)
+
+     tk.Label(ventana, text="Cargo: ",font=("Calibri", 10), bg="gray").pack()
+     global en_cargo
+     en_cargo=tk.Entry(ventana, show="*", font=("Calibri", 10))
+     en_cargo.pack(pady=8)
 
      boton_registrar = tk.Button(ventana, text="Registrar", command=registrar_usuario)
      boton_registrar.pack(pady=10)

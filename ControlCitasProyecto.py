ef5bbc8e37dd98911a3be65079b0faf8b6851559
DeLaCruzHernandezJessica <jessica.delacruz.cbtis03@gmail.com>
2025-05-31 16:34:11 -0600

    ControlCitasProyecto.py

--- ControlCitasProyecto.py
+++ ControlCitasProyecto.py
@@ -45,18 +45,23 @@ class registrar:
         return f"{self.nombre} {self.apellido1} {self.apellido2} a sido registrado exitosamente"
 
 def registrar_usuario():
- nombre = en_nombre.get()
- apellido1 = en_apellido1.get()
- apellido2 = en_apellido2.get()
- curp = en_curp.get()
- sexo = en_sexo.get()
- edad = en_edad.get()
- telefono = en_telefono.get()
- cargo = en_cargo.get()
+ try:
+     nombre = en_nombre.get()
+     apellido1 = en_apellido1.get()
+     apellido2 = en_apellido2.get()
+     curp = en_curp.get()
+     sexo = valor.get()
+     edad = en_edad.get()
+     telefono = en_telefono.get()
+     cargo = en_cargo.get()
+     if not all([nombre, apellido1, apellido2, curp, sexo, edad, telefono, cargo]):
+        raise ValueError("Todos los campos son obligatorios")
+     registro = registrar(nombre, apellido1, apellido2, curp, sexo, edad, telefono, cargo)
+     mensaje = registro.datos()
+     messagebox.showinfo("Datos Registrados", mensaje)
 
- registro = registrar(nombre, apellido1, apellido2, curp, sexo, edad, telefono, cargo)
- mensaje = registro.datos()
- messagebox.showinfo("Datos Registrados", mensaje)
+ except ValueError as e:
+    messagebox.showerror("error, no se pudo completar el registro, porfavor intente de nuevo")
 
 def mostrar_formulario(event):
     seleccion = combo.get()
@@ -68,41 +73,43 @@ def mostrar_formulario(event):
 
     if seleccion == "Registrarse":
 
-     tk.Label(formulario_frame,text="Nombre: ", font=("Calibri", 10), bg="gray").pack(pady=0)
+     tk.Label(formulario_frame,text="Nombre: ", font=("Calibri", 10), bg="gray").grid(pady=10, padx=10, sticky="e")
      en_nombre=tk.Entry(formulario_frame, font=("Calibri", 10))
-     en_nombre.pack(pady=8)
+     en_nombre.grid(row=0, column=1, padx=10, pady=10)
 
-     tk.Label(formulario_frame, text="Apellido paterno: ", font=("Calibri", 10), bg="gray").pack()
+     tk.Label(formulario_frame, text="Apellido paterno: ", font=("Calibri", 10), bg="gray").grid(pady=10, padx=10, sticky="e")
      en_apellido1=tk.Entry(formulario_frame, font=("Calibri", 10))
-     en_apellido1.pack(pady=8)
+     en_apellido1.grid(row=1, column=1, padx=10, pady=10)
 
-     tk.Label(formulario_frame, text="Apellido materno: ", font=("Calibri", 10), bg="gray").pack()
+     tk.Label(formulario_frame, text="Apellido materno: ", font=("Calibri", 10), bg="gray").grid(pady=10, padx=10, sticky="e")
      en_apellido2=tk.Entry(formulario_frame, font=("Calibri", 10))
-     en_apellido2.pack(pady=8)
+     en_apellido2.grid(row=2, column=1, padx=10, pady=10)
 
-     tk.Label(formulario_frame, text="CURP: ",font=("Calibri", 10), bg="gray").pack()
+     tk.Label(formulario_frame, text="CURP: ",font=("Calibri", 10), bg="gray").grid(pady=10, padx=10, sticky="e")
      en_curp=tk.Entry(formulario_frame, font=("Calibri", 10))
-     en_curp.pack(pady=8)
+     en_curp.grid(row=3, column=1, padx=10, pady=10)
 
-     tk.Label(formulario_frame, text="Sexo:", bg="gray").pack()
+     tk.Label(formulario_frame, text="Sexo:", bg="gray").grid(pady=10, padx=10, sticky="e")
      valor = tk.StringVar()
+     valor.set("Seleccionar")
      en_sexo = tk.OptionMenu(formulario_frame, valor, "Masculino", "Femenino", "Otro")
-     en_sexo.pack()
+     en_sexo.grid(row=4, column=1, padx=10, pady=10)
      
-     tk.Label(formulario_frame, text="Edad: ",font=("Calibri", 10), bg="gray").pack()
+     tk.Label(formulario_frame, text="Edad: ",font=("Calibri", 10), bg="gray").grid(pady=10, padx=10, sticky="e")
      en_edad=tk.Entry(formulario_frame, font=("Calibri", 10))
-     en_edad.pack(pady=8)
+     en_edad.grid(row=5, column=1, padx=10, pady=10)
 
-     tk.Label(formulario_frame, text="Telefono: ",font=("Calibri", 10), bg="gray").pack()
+     tk.Label(formulario_frame, text="Telefono: ",font=("Calibri", 10), bg="gray").grid(pady=10, padx=10, sticky="e")
      en_telefono=tk.Entry(formulario_frame, show="*", font=("Calibri", 10))
-     en_telefono.pack(pady=8)
+     en_telefono.grid(row=6, column=1, padx=10, pady=10)
 
-     tk.Label(formulario_frame, text="Cargo: ",font=("Calibri", 10), bg="gray").pack()
+     tk.Label(formulario_frame, text="Cargo: ",font=("Calibri", 10), bg="gray").grid(pady=10, padx=10, sticky="e")
      en_cargo=tk.Entry(formulario_frame, show="*", font=("Calibri", 10))
-     en_cargo.pack(pady=8)
+     en_cargo.grid(row=7, column=1, padx=10, pady=10)
 
      boton_registrar = tk.Button(formulario_frame, text="Registrar", command=registrar_usuario)
-     boton_registrar.pack(pady=10)
+     boton_registrar.grid(row=8, column=1, padx=10, pady=10)
+
     elif seleccion == "Agendar cita":
      tk.Label(formulario_frame,text="Nombre: ", font=("Calibri", 10), bg="gray").pack(pady=0)
      en_nombre=tk.Entry(formulario_frame, font=("Calibri", 10))
@@ -134,6 +141,10 @@ def mostrar_formulario(event):
      
      tk.Button(formulario_frame, text="Agendar Cita", command=agendar).pack(pady=10)
 
+     
+
+       
+
 tk.Label(ventana, text="¿Que quieres hacer?", bg="gray", font=("Calibri", 20)).pack(pady=5)
 combo = ttk.Combobox(ventana, values=["Registrarse", "Agendar cita", "Afiliarse", "ingresar (acceso solo a personal)"], font=("Calibri, 15"))
 combo.pack(pady=5)
@@ -144,4 +155,6 @@ formulario_frame.pack(pady=20)
 
 combo.bind("<<ComboboxSelected>>", mostrar_formulario)
 
+
 ventana.mainloop()
+
